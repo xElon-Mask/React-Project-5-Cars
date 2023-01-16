37bde290c526004923049480934f58b9df0080aa
xElon-Mask <xelonmask@protonmail.com>
2023-01-16 12:19:08 +0100

    creating components Mycars, Cars

---
+++ src/components/Cars.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Cars extends Component {
+  render() {
+    return (
+      <div>
+        <p>Marque:</p>
+        <p>Couleur:</p>
+      </div>
+    );
+  }
+}
+
+export default Cars;
