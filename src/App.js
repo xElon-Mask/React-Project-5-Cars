ce0e692d9f4305e2ddd23b444f7a5a83dc44e202
xElon-Mask <xelonmask@protonmail.com>
2023-01-17 21:16:06 +0100

    add events and params to modify state

--- src/App.js
+++ src/App.js
@@ -8,10 +8,42 @@ class App extends Component {
     color: "green",
   };
 
+  changeTitle = (e) => {
+    console.log(e.target);
+    this.setState({
+      titre: "Titre MAJ grâce à la méthode setState",
+    });
+  };
+
+  changeViaParam = (titre) => {
+    this.setState({
+      titre: titre,
+    });
+  };
+
+  changeViaBind = (param) => {
+    this.setState({
+      titre: param,
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <Mycars color={this.state.color} title={this.state.titre} />
+
+        <button onClick={this.changeTitle}>Changer le nom en dur</button>
+        <button onClick={() => this.changeViaParam("I love Tesla")}>
+          Regarde ma seedphrase
+        </button>
+        <button
+          onClick={this.changeViaBind.bind(
+            this,
+            "Le titre est changée grâce à la méthode bind"
+          )}
+        >
+          Surprise
+        </button>
       </div>
     );
   }
--- src/components/Mycars.js
+++ src/components/Mycars.js
@@ -1,20 +1,33 @@
 import React, { Component } from "react";
 import Car from "./Cars";
-import Wrapper from "./Wrapper";
-import MyHeader from "./MyHeader";
+//import Wrapper from "./Wrapper";
+//import MyHeader from "./MyHeader";
 
 class Mycars extends Component {
   state = {
     cars: ["Nissan", "Volvo", "Tesla"],
   };
+
+  noCopy = () => {
+    alert("Je vous avais prévenu !");
+  };
+
+  addStyle = (e) => {
+    if (e.target.classList.contains("styled")) {
+      e.target.classList.remove("styled");
+    } else {
+      e.target.classList.add("styled");
+    }
+  };
+
   render() {
-    const { title, color } = this.props;
+    const { title } = this.props;
     return (
       <div>
-        <Wrapper>
-          <MyHeader myStyle={color}>{title}</MyHeader>
-        </Wrapper>
-
+        <h1 onMouseOver={this.addStyle}>{title}</h1>
+        <p onCopy={this.noCopy}>
+          Ceci est un paragraphe qu'il ne faut surtout pas copier
+        </p>
         <Car color="Noire">{this.state.cars[0]}</Car>
         <Car color="Rouge">{this.state.cars[1]}</Car>
         <Car color="Bleue">{this.state.cars[2]}</Car>
